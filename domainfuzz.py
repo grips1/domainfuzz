dae2989748c9d22a79e8366d2911b1d1ee0d384d
fendermcbender <55134864+grips1@users.noreply.github.com>
2024-11-25 09:12:39 +0000

    beta
    only preceding fuzzing, fuzzcount input, tld fuzz

--- domainfuzz.py
+++ domainfuzz.py
@@ -1,64 +1,84 @@
 #!/usr/bin/python3
 
-#domain fuzzer
-#keywords = cyber, sploit, 0day, l33t
-#test it with 1 chracter of ?a and ?d "cybera, cyberb, cyber1, etc." DONE
-#need to find python summary file with syntax
-#check for "domain for sale" false positives
-#requests.get throws an error if the protocol specified doesn't exist (http(s))
+### Tasks ###
+
+#    Test for domain registrar pages via page content and skip them.
+#    Add line to input length of fuzzing and prefix/suffix/bothfix (both at the same time)/prefix-then-suffix (do both separately) mode as CLI parameters.
+#    Some requests hang for too long, set a time limit for the requests.
+#	 Seems like I'll need to make the character-adding feature a function rather than a loop, in order to implement variable length fuzzing
+#!	 No need to provide character optionality... just fuzz numbers and letters altogether
+# 	 python3 -c [fuzz length] -o [output filename] 
 import requests
+
+#!   import argparse
+#!   parser = argparse.ArgumentParser(prog="DomFuzz",description="A domain fuzzer for discovering cryptic, hidden domains",epilog="Read the help dumbass")
 
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 numbers = ['0','1','2','3','4','5','6','7','8','9']
-keywords = ["https://books", "https://audiobooks", "https://audio", "https://book", "https://epub"]
-new_word = "temp"
+tlds = ['.com', '.us', '.wtf','.org']
+tlds_index = 3 #array length for fuzz()
+
+domains = ["hack"]
+
+new_word = ""
 
 def send_request(domain, fd):
 	header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; rv:128.0) Gecko/20100101 Firefox/128.0'}
 	try:
 		print(domain)
-		request = requests.get(domain)
+		request = requests.get(domain, timeout=0.5)
 	except:
 		return -1
 	else: 
 		if request.status_code == 200:
 			fd.write(domain + '\n')
-			print("############\n###FOUND!###\n############")
+			print("\n############\n###FOUND!###\n############\n")
 
-def concatenate(domain, iteration, index):
-	if iteration == 1:
+def fuzz(domain, iteration, index, tld_index):
+	domain = "https://" + domain
+	if iteration == 0:
 		domain += alphabet[index]
-	elif iteration == 2:
+	elif iteration == 1:
 		domain += numbers[index]
-	
-	domain += ".com"
+	# Top Level Domain loop logic
+
+	domain += tlds[tld_index]
 	return domain
-#to implement more than one ?l, I could nest the for loop twice, but then how do I add even more ?l's? Cant have a loop for each letter
 
 def main():
+	fuzzcount = input("char # to fuzz: ")
+	fuzzcount = int(fuzzcount)
 	fd = open('fuzzed3', 'w')
-	for word in keywords:
-		i = 0
-		while i < 26: #alphabet
-			new_word = concatenate(word, 1, i)
-			if send_request(new_word, fd) == -1:
-				print("Not found!")
-			i += 1
-		i = 0
-		while i < 10: #numbers
-			new_word = concatenate(word, 2, i)
-			if send_request(new_word, fd) == -1:
-				print("NOT FOUND!")
-			i += 1
-
+	for domain in domains:
+		while fuzzcount > 0:
+			fuzzcount_int = fuzzcount
+			i = 0
+			print("Starting alphabet fuzzing...")
+			while i < 26: #alphabet
+				loop_tld_index = 0
+				while loop_tld_index < 4:
+					new_word = fuzz(domain, 0, i, loop_tld_index)
+					if send_request(new_word, fd) == -1:
+						print("Nope...")
+					loop_tld_index += 1
+				i += 1
+			i = 0
+			print("Starting number fuzzing...")
+			while i < 10: #numbers
+				loop_tld_index = 0
+				while loop_tld_index < 4:
+					new_word = fuzz(domain, 1, i, loop_tld_index)
+					if send_request(new_word, fd) == -1:
+						print("Nope...")
+					loop_tld_index += 1 
+				i += 1
+			fuzzcount -= 1
 	fd.close()
 
 if __name__ == '__main__':
 	main()
 
-"""		while i < 10: #numbers
-			send_request(new_word)
-			i += 1
-"""
 
-#somewhere down the line, need to have: fd.close()
+# Add https:// automatically to the beginning of the domain DONE
+# TODO -- Work on trailing fuzzing first, then implement both. 
+#^ Need to add variable length fuzzing.
